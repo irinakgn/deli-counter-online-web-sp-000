178f0155c4ab88b66e1df075df91e893e844ba10
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 13:01:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(katz_deli)
   message = ''
   if katz_deli.length == 0
-     message "The line is currently empty."
+     message = "The line is currently empty."
    end
 
    puts message
