63c5327663d266158ec09577d423d915fc304142
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 12:53:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,17 @@
-def line()
- puts "The line is currently empty."
+def line(katz_deli)
+  message = ''
+  if katz_deli.length == 0
+    message = "The line is currently empty."
+ elsif katz_deli.length > 0
+   message = "The line is currently: "
+   katz_deli.each_with_index do |i, n|
+      message + "#{i} #{n}"
+   end
+
+   return message
 end
 
 
 def take_a_number(katz_deli, name)
+
 end
