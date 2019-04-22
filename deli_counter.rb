f2e578618c4146e6fd4aa4801d5e2c5506408a45
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 13:11:41 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,28 @@
+def line(katz_deli)
+  message = ''
+  if katz_deli.length == 0
+    message = "The line is currently empty."
+  elsif katz_deli.length > 0
+    message = "The line is currently:"
+    katz_deli.each_with_index { |item, index| message += " #{index+1}. #{item}" }
+  end
+
+  puts message
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  katz_deli
+end
+
+def now_serving(katz_deli)
+  message = ''
+  if katz_deli.length == 0
+    message = "There is nobody waiting to be served!"
+  elsif katz_deli.length > 0
+    a = katz_deli.shift()
+    message = "Currently serving #{a}."
+  end
+
+  puts message
+end
