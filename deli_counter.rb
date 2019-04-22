0405a77033a4aa33e991587b41b95d6325e5cbcd
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 12:56:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 def line(katz_deli)
-  message = ''
-  if katz_deli.length == 0
-    message = "The line is currently empty."
-  elsif katz_deli.length > 0
-   message = "The line is currently: "
-   katz_deli.each_with_index do |i, n|
-      # message += "#{i} #{n}"
-  end
+  # message = ''
+  # if katz_deli.length == 0
+  #   message = "The line is currently empty."
+  # elsif katz_deli.length > 0
+  #  message = "The line is currently: "
+  #  katz_deli.each_with_index do |i, n|
+  #     # message += "#{i} #{n}"
+  # end
 
   #  return message
 end
