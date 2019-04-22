559b448e1191a4bf6cf2c90f59df697ef94f2959
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 12:53:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
  elsif katz_deli.length > 0
    message = "The line is currently: "
    katz_deli.each_with_index do |i, n|
-      message = message + "#{i} #{n}"
+      message += "#{i} #{n}"
    end
 
    return message
